323921521f8c1cb584c7a007c1ba8c8311f4e466
nafistarik <nafistarik789@gmail.com>
2025-07-17 16:37:36 +0600

    fix: ignored hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`antialiased`} suppressHydrationWarning={true}>
         <ReduxProvider>{children}</ReduxProvider>
         <Toaster position="top-right" richColors />
       </body>
