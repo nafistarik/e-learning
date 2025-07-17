2bf7fd457375adcfabe561e312b011f9111e9bed
nafistarik <nafistarik789@gmail.com>
2025-07-17 16:49:18 +0600

    tr can not be parent of some html tags

--- components/motion/FadeUp.tsx
+++ components/motion/FadeUp.tsx
@@ -13,7 +13,7 @@ const FadeUp = ({
   duration?: number;
 }) => {
   return (
-    <motion.tr
+    <motion.div
       initial={{ opacity: 0, y: 10 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{
@@ -24,7 +24,7 @@ const FadeUp = ({
       className=" transition-colors"
     >
       {children}
-    </motion.tr>
+    </motion.div>
   );
 };
 
